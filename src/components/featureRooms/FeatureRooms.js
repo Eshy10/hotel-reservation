2252ae1685c1c817e39500e5081ee52cd83ebfad
Eshy10 <adewalebose15@gmail.com>
2020-11-04 02:18:14 +0100

    add loading component

--- src/components/featureRooms/FeatureRooms.js
+++ src/components/featureRooms/FeatureRooms.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Title from '../title/Title';
+import { RoomContext } from '../../context';
+import Loading from '../Loading';
+import Room from '../rooms/Room';
+import styles from './featureRooms.module.css';
 
-const FeatureRooms = () => (
-  <div>
-    Hi i am passing the value of
-  </div>
-);
+const FeatureRooms = () => {
+  const { loading, featuredRooms } = useContext(RoomContext);
+  console.log(featuredRooms);
+  return (
+    <section className={styles.featuredRooms}>
+      <Title title="featured rooms" />
+      <div className={styles.featuredRoomsCenter}>
+        {
+        loading ? <Loading />
+          : featuredRooms.map(room => (
+            <Room key={room.id} room={room} />
+          ))
+      }
+      </div>
+    </section>
+  );
+};
 
 export default FeatureRooms;
---
+++ src/components/rooms/Room.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Room = () => (
+  <div>
+    Hello from room
+  </div>
+);
+
+export default Room;
